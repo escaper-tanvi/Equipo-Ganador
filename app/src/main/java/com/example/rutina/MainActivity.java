10d4274d7b990e10a7e393737edd9f87e5af40cf
escaper-tanvi <t_mathur@ece.iitr.ac.in>
2022-02-26 20:23:00 +0530

    Implements splash screen with sample design

--- app/src/main/java/com/example/rutina/MainActivity.java
+++ app/src/main/java/com/example/rutina/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.rutina;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements onCreate {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
